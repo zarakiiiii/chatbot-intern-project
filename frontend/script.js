3961bec4ecf9cdf6b130e790fc73364e386ca5d6
Yash Shambharkar <yyyyash123@gmail.com>
2025-09-21 09:38:17 +0530

    Updated script.js with backend API link

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = "https://chatbot-intern-project.onrender.com/api";
 
 const messagesEl = document.getElementById('messages');
 const inputEl = document.getElementById('userInput');
